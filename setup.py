3bfc3ca6d4f66f0a182d9730f203bc450c82cdb4
Jan Ripke <janripke@gmail.com>
2021-03-13 17:01:19 +0100

    correct paprika-connector dependency

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         'click',
         'psycopg2-binary',
         'requests',
-        'paprika-connector',
+        'paprika-connector==0.0.3',
         'pydub'
     ],
     dependency_links=[
