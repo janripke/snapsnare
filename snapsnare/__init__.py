4dcc5b39478528723a5066e2d9edab9e688f474b
Jan Ripke <janripke@gmail.com>
2021-03-21 17:58:32 +0100

    branch for nickname and phonenumber

--- snapsnare/__init__.py
+++ snapsnare/__init__.py
@@ -5,8 +5,8 @@ __description__ = 'snapsnare the sample base for musicians'
 __uri__ = 'https://github.com/janripke/snapsnare.git'
 __doc__ = __description__ + ' <' + __uri__ + '>'
 
-__author__ = 'Jan Ripke'
-__email__ = 'janripke@gmail.com'
+__author__ = 'Jan Ripke;Paulo Marta'
+__email__ = 'janripke@gmail.com;pcmarta1975@gmail.com'
 
 __license__ = 'GNU General Public License v3 or later (GPLv3+)'
-__copyright__ = 'Copyleft (l) 2016-2020 Jan Ripke'
+__copyright__ = 'Copyleft (l) 2016-2020 snapsnare'
