d9a6ce4b0e19c67a0e11d413bd17ee4331bd6cd6
Jan Ripke <janripke@gmail.com>
2022-01-25 20:01:09 +0100

    reset modifications

--- snapsnare/repositories/reset/reset_repository.py
+++ snapsnare/repositories/reset/reset_repository.py
@@ -1,10 +1,7 @@
 from snapsnare.repositories.dataclass_repository import DataclassRepository
 
 
-class UserRepository(DataclassRepository):
+class ResetRepository(DataclassRepository):
 
     def __init__(self, connector):
         DataclassRepository.__init__(self, connector, 'resets')
-
-    def find_by_username(self, username):
-        return self.find_by(username=username, active=1)
--- snapsnare/templates/password_forgotten/password_forgotten.py
+++ snapsnare/templates/password_forgotten/password_forgotten.py
@@ -1,4 +1,4 @@
-from flask import session
+from uuid import uuid4
 from flask import Blueprint
 from flask import flash
 from flask import render_template
@@ -6,13 +6,10 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import current_app
-from flask_login import login_user
 
 from snapsnare.repositories.user.user_repository import UserRepository
 from snapsnare.repositories.section.section_repository import SectionRepository
-from snapsnare.system import dictionaries
-from snapsnare.templates.login.user import User
-from snapsnare.system import hasher
+from snapsnare.repositories.reset.reset_repository import ResetRepository
 
 
 password_forgotten = Blueprint('password_forgotten', __name__, template_folder='templates')
@@ -28,13 +25,29 @@ def show():
         connector.close()
         return render_template('password_forgotten/password_forgotten.html', sections=sections)
 
-    username = request.form['username']
+    if request.method == 'POST':
+        username = request.form['username']
 
-    user_repository = UserRepository(connector)
-    user = user_repository.find_by_username(username)
+        user_repository = UserRepository(connector)
+        user = user_repository.find_by(username=username, active=1)
 
-    if not user:
-        flash('This username is not registered', 'danger')
-        return redirect(url_for('register.show'))
+        if not user:
+            flash('Invalid account state', 'danger')
+            return redirect(url_for('login.show'))
 
-    return redirect(request.args.get('next') or url_for('login.show'))
+        # disable previous resets of this user.
+        reset = {
+            "active": 0
+        }
+        reset_repository = ResetRepository(connector)
+        reset_repository.update(reset, where={"username": user["username"]})
+
+        # create an active reset for this user.
+        reset = {
+            "active": 1,
+            "uuid": str(uuid4()),
+            "username": user["username"],
+        }
+        reset_repository.insert(reset)
+
+        return redirect(url_for('login.show'))
